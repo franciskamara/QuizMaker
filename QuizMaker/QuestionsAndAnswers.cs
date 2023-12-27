72d963ec600278701f9be60c4f6759602ecba4ee
Francis Kamara <franciskamara@hotmail.co.uk>
2023-12-27 12:47:44 +0000

    QuestionsAndAnswers.cs update

--- QuizMaker/QuestionsAndAnswers.cs
+++ QuizMaker/QuestionsAndAnswers.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿
 namespace QuizMaker
 {
-	public class QuestionsAndAnswers
-	{
-		public QuestionsAndAnswers()
-		{
-		}
-	}
+    public class QuestionsAndAnswers
+    {
+
+        public string Question;
+
+        public string answerOne;
+        public string answerTwo;
+        public string answerThree;
+        public string answerFour;
+        public string correctAnswer;
+        
+    }
 }
+
+
 
