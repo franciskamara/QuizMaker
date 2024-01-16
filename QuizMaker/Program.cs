f6c3a885c810bac89e5de7c9666735bcdd3c10ac
Francis Kamara <franciskamara@hotmail.co.uk>
2024-01-16 19:18:02 +0000

    WIP: Deserialiser

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System.Net;
+using System.Xml.Serialization;
 
 namespace QuizMaker;
 
@@ -9,47 +10,50 @@ public class Program
         Console.WriteLine("Welcome to the Quiz Maker");
 
         Console.Write("How many questions do you wish to include? ");
-        int numberOfQuestions = int.Parse(Console.ReadLine());
+        int numberOfQuestions = int.Parse(Console.ReadLine()); //Quantity of questions to input
         Console.Write("How many answers do you want to include for your questions? ");
-        int numberOfAnswers = int.Parse(Console.ReadLine());
+        int numberOfAnswers = int.Parse(Console.ReadLine()); //Quantity of answer options to input 
         Console.Clear();
-            
-        List<QuestionsAndAnswers> qList = new();
 
-        for (int qInput = 0; qInput < numberOfQuestions; qInput++)//Loops for Q and A input
+        List<QuestionsAndAnswers> qList = new(); //Create a new Instance (List) of QuestionsAndAnswers object/class
+
+        for (int qInput = 0; qInput < numberOfQuestions; qInput++) //Loop: question input
         {
-            
             QuestionsAndAnswers qAndA = new();
             Console.Write("Input the question: ");
             qAndA.Question = Console.ReadLine();
             Console.Clear();
 
-            for (int aInput = 0; aInput < numberOfAnswers; aInput++)
+            for (int aInput = 0; aInput < numberOfAnswers; aInput++) //Loop: answers input
             {
-                Console.Write($"Input answer {aInput +1}: ");
-                qAndA.answers.Add( Console.ReadLine());
+                Console.Write($"Input answer {aInput + 1}: ");
+                qAndA.answers.Add(Console.ReadLine());
                 Console.Clear();
-                
             }
 
             Console.Write("Choose the correct answer index: ");
-            qAndA.correctAnswer = Int32.Parse(Console.ReadLine());
-            
-            qList.Add(qAndA);
-            Console.WriteLine(qAndA.Question);
+            qAndA.correctAnswer = Int32.Parse(Console.ReadLine()); //Correct answer index input
+            Console.Clear();
 
-            foreach (string answer in qAndA.answers)
+            qList.Add(qAndA); //Add instance
+            Console.WriteLine(qAndA.Question); //Print question
+
+            foreach (string answer in qAndA.answers) //Print each answer inputted
             {
-                Console.WriteLine(answer);   
+                Console.WriteLine(answer);
             }
 
-            XmlSerializer writer = new XmlSerializer(typeof(QuestionsAndAnswers));
-            var path = @"QuestionsAndAnswersList.xml";
-            using (FileStream file = File.Create(path))
+            XmlSerializer serializer = new XmlSerializer(typeof(QuestionsAndAnswers)); //Serialize instance data
+            var path = @"QuestionsAndAnswersList.xml"; //Naming
+            using (FileStream file = File.Create(path)) //Write data in path file
             {
-                writer.Serialize(file, qAndA);
+                serializer.Serialize(file, qAndA);
             }
 
+            using (FileStream file = File.OpenRead(path))//Deserializing instance
+            {
+                qAndA = serializer.Deserialize(file) as QuestionsAndAnswers;
+            }
         }
     }
 }
