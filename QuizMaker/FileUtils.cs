27ccf1c7453d6dbc634315f1f52b1cd531115a7c
Francis Kamara <franciskamara@hotmail.co.uk>
2024-01-26 13:18:26 +0000

    WIP: Serialisation class

--- QuizMaker/FileUtils.cs
+++ QuizMaker/FileUtils.cs
@@ -4,19 +4,30 @@ namespace QuizMaker;
 
 public class FileUtils
 {
-    public static void xmlSerializer()
+    public static void SaveData(QuestionsAndAnswers FileQuesAndAnswers)
     {
-        QuestionsAndAnswers qAndA = new();
+        
         XmlSerializer serializer = new XmlSerializer(typeof(QuestionsAndAnswers)); //Serialize instance data
         var path = @"QuestionsAndAnswersList.xml"; //Naming
         using (FileStream file = File.Create(path)) //Write data in path file
         {
-            serializer.Serialize(file, qAndA);
+            serializer.Serialize(file, FileQuesAndAnswers);
         }
         
+        
+    }
+
+    public static QuestionsAndAnswers LoadData()
+    {
+        
+        QuestionsAndAnswers returnValue;
+        XmlSerializer deserializer = new XmlSerializer(typeof(QuestionsAndAnswers));
+        string path = @"QuestionsAndAnswersList.xml"; //Naming
         using (FileStream txtFile = File.OpenRead(path)) //Deserializing instance
         {
-            qAndA = serializer.Deserialize(txtFile) as QuestionsAndAnswers;
+            returnValue = deserializer.Deserialize(txtFile) as QuestionsAndAnswers;
         }
+
+        return returnValue;
     }
 }
--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -20,38 +20,23 @@ public class Program
             int numberOfQuestions = UIMethods.InputNumberOfQuestions();
             int numberOfAnswers = UIMethods.InputNumberOfAnswers();
 
-            List<QuestionsAndAnswers> qList = new(); //Create a new Instance (List) of QuestionsAndAnswers object/class
-
+            List<QuestionsAndAnswers> ListQuesAndAnswers= new(); //Create a new Instance (List) of QuestionsAndAnswers object/class
+            QuestionsAndAnswers FileQuesAndAnswers = new();
+            
             for (int qInput = 0; qInput < numberOfQuestions; qInput++) //Loop: question input
             {
-                QuestionsAndAnswers qAndA = new();
 
-                qAndA.question = UIMethods.InputQuestion();
+                FileQuesAndAnswers.question = UIMethods.InputQuestion();
 
-                qAndA.answers = UIMethods.InputAnswers(numberOfAnswers);
+                FileQuesAndAnswers.answers = UIMethods.InputAnswers(numberOfAnswers);
 
-                qAndA.correctAnswer = UIMethods.InputCorrectAnswerIndex();
+                FileQuesAndAnswers.correctAnswer = UIMethods.InputCorrectAnswerIndex();
 
-                qList.Add(qAndA); //Add questions and answers/answer to list
-
-                XmlSerializer serializer = new XmlSerializer(typeof(QuestionsAndAnswers)); //Serialize instance data
-                var path = @"QuestionsAndAnswersList.xml"; //Naming
-                using (FileStream file = File.Create(path)) //Write data in path file
-                {
-                    serializer.Serialize(file, qAndA);
-                }
-
-                using (FileStream txtFile = File.OpenRead(path)) //Deserializing instance
-                {
-                    qAndA = serializer.Deserialize(txtFile) as QuestionsAndAnswers;
-                }
-
-                // Console.WriteLine(qAndA.Question); //Print question
-                // foreach (string answer in qAndA.answers) //Print each answer 
-                // {
-                //     Console.WriteLine(answer);
-                // }
+                ListQuesAndAnswers.Add(FileQuesAndAnswers); //Add questions and answer(s) to list
+                
+                
             }
+            FileUtils.SaveData(FileQuesAndAnswers);
         }
 
         if (userSelection == playOption)
