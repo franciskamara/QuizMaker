dfaf72ae5d91f9864973155df61d45f70d41898c
Francis Kamara <franciskamara@hotmail.co.uk>
2024-02-04 20:29:11 +0000

    WIP: random generator

--- QuizMaker/LogicMethods.cs
+++ QuizMaker/LogicMethods.cs
@@ -4,17 +4,16 @@ public class LogicMethods
 {
     public static QuestionsAndAnswers RandomQuestionGenerator(List<QuestionsAndAnswers> loadedQuesAndAnswers)
     {
-        QuestionsAndAnswers randomQuestion = null;
+        QuestionsAndAnswers randomQuestion = null;//Print random Q&A 
 
         if (loadedQuesAndAnswers != null && loadedQuesAndAnswers.Count > 0)
         {
-            Random random = new Random();
-            randomQuestion = loadedQuesAndAnswers[random.Next(loadedQuesAndAnswers.Count)];
+            Random rng = new Random();
+            randomQuestion = loadedQuesAndAnswers[rng.Next(loadedQuesAndAnswers.Count)];
         }
         else
         {
             Console.WriteLine("No questions available. Please add questions before playing.");
-            // You may want to handle this case appropriately, e.g., allow the user to add questions
         }
 
         return randomQuestion;
